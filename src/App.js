26274b7ea0e21743478b5a919d4a3dd4e3a5fa5f
Adrian Li <li.adrianmc@gmail.com>
2019-02-09 12:36:32 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h1>Welcome to React Parcel Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+  </div>
+);
