284cc53b910efc12511c1bc2e3ccf46a1d67715d
Param Choudhary <69947192+Paramchoudhary@users.noreply.github.com>
2021-08-30 22:44:28 +0530

    hello world

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />
+                <h1>hello world</h1>, document.getElementById("root"));
