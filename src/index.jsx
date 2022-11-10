891b8e3d9b0bf8253f45431217ee065b597ab05f
Marcus Greasly <mgreasly@hotmail.com>
2022-11-10 11:12:29 +1000

    Tidied up the createElement call

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,4 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import App from "./App";
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+createRoot(document.getElementById('root')).render(<App/>)
